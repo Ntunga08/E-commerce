08a0962a3745180b11a27d8790e7d9b17fde73d5
ntunga08 <samwelbuluba22@gmail.com>
2025-08-04 23:25:47 +0300

    added feature on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
 
 import React from 'react';
 import Navbar from '../components/Navbar';
+import barcaImage from '../assets/barca.jpg'; 
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="p-3">
-        <h1 className="text-3xl font-bold">BULUBA_SPORTWEARS</h1>
+        <h1 className="text-3xl font-bold ">BULUBA_SPORTWEARS</h1>
+        <div className='rounded-3xl  border-gray overflow-hidden bg-white inset-shadow-xl hover:shadow-md transition w-64 mt-4'>
+          <div className='relative'>
+            <img src={barcaImage} alt="Product" className="w-full h-48 object-cover mb-4" />
+  
+            <span className="absolute top-2  bg-gray bg-opacity-60 text-black text-xs px-2 py-1 rounded">Guest favorite</span>
+          </div>
+          <div className='p-4'>
+            <h3 className='font-semibold text-lg'>Manchester United</h3>
+            <p className='text-gray-600 mt-1'>$147 for 2 nights  5.0</p>
+          </div>    
+        </div>
       </div>
     </>
   );
 }
-
 export default App;
